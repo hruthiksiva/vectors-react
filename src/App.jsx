9a7d84fb00760a3e2741782862b67b11ce07cb15
Mukundan <manojmukundan4@gmail.com>
2024-12-25 23:43:03 +0530

    Add other Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import MainLayout from './Layouts/MainLayout'
 import Homepage from './Pages/Home'
-
+import ServicePage from './Pages/ServicePage'
+import Portfolio from './Pages/Portfolio'
+import About from './Pages/About'
+import ContactUs from './Pages/ContactUs'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route path='/' element={<Homepage/>}/>
+      <Route path='/service' element={<ServicePage/>}/>
+      <Route path='/Portfolio' element={<Portfolio/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/ContactUs' element={<ContactUs/>}/>
     </Route>
   )
 )
