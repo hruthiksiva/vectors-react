c6d127eebcb30e258ae4f24746758ff9d28487ee
Mukundan <manojmukundan4@gmail.com>
2024-12-29 18:30:38 +0530

    Add Corestrength section in the about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,27 +1,21 @@
 import React from 'react'
 import Story from '../Components/Story'
 import Mission from '../Components/Mission'
-import Extrasss from '../Components/Vision'
 import Testimonials from '../Components/Testimonials'
 import ContactCard from '../Components/ContactCard'
+import CoreStrength from '../Components/CoreStrength'
+
+
 
 const About = () => {
   return (
     <>
-      <Story/>
-      <Mission/>
-        <Testimonials/>
-      {/* <Extrasss/>  */}
-
-      {/* corestrength */}
-  <div class="card border-0 " style={{width: "18rem", backgroundColor:'lightyellow'}}>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    </div>
-      <ContactCard/>
+      <Story />
+      <Mission />
+      <Testimonials />
+      {/* Core Strengths Section */}
+      <CoreStrength/>
+      <ContactCard />
     </>
   )
 }
