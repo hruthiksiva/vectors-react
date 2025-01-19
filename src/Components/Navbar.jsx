edcd76c0ede37ddd75f96108dd1973532c10f63e
Mukundan <manojmukundan4@gmail.com>
2025-01-19 21:12:04 +0530

    Make some adjustements to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid mx-5 px-5">
+      <div className="container-fluid mx-lg-5 px-lg-5">
         <Link className="navbar-brand" to="/">
           <img src={logoimg} className="img-fluid" style={{ maxHeight: '45px' }} /> Vectors
         </Link>
@@ -32,8 +32,21 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+          
             <li className="nav-item">
-              <Link className="nav-link mx-2" aria-current="page" to="/">Home</Link>
+            <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+              <Link className="nav-link ms-2" aria-current="page" to="/">Home</Link>
+               
             </li>
             <li className="nav-item">
               <Link className="nav-link mx-2" to="/about">About</Link>
@@ -65,6 +78,7 @@ const Navbar = () => {
           <form className="d-flex" role="search">
             <Link to="/contact" className="btn btn-outline-success">Contact</Link>
           </form>
+          
         </div>
       </div>
     </nav>
